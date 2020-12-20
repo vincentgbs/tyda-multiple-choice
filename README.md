72abe480d5e7b30250d621ba9b15105148963f29
vincent <vincent.gbs11@gmail.com>
2020-12-20 10:08:54 -0500

    Moving around instructions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-A simple Multiple Choice Plugin for Wordpress
+A simple Lesson Building Plugin for Wordpress
+
+The 'question' post needs subfields: 'answer', 'option1', 'option2', 'option3', 'next_question', 'previous_question'. The 'answers' field must be populated, while the others are optional.
+Optionally, you can add the 'question_id' to the 'correct' and 'wrong' post types to track them in the backend.
+
+The 'lesson' taxonomy needs the subfields: 'week_open', 'week_due'. These correspond to the dates that the lesson will be available with respect to the semester start.
+The members plugin is needed to add the 'student', 'alumni', 'teacher' roles.
--- tyda-multiple-choice.php
+++ tyda-multiple-choice.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Tyda Multiple Choice Plugin
   Plugin URI: https://github.com/vincentgbs/tyda-multiple-choice
-  Description: This plugin requires the advanced-custom-fields plugin. The 'question' post needs subfields: 'option1', 'option2', 'option3', 'answer', 'next_question', 'previous_question'. You can optionally add the 'question_id' to the 'correct' and 'wrong' post types to track them in the backend. The members plugin is needed to add the 'student', 'alumni', 'teacher' roles.
+  Description: This plugin requires the advanced-custom-fields plugin and the members plugin. This is a simple lesson creator with multiple choice answers.
   Version: 0.1
   Author: Vincent Hu
   Author URI: https://www.vincenthu.dev
